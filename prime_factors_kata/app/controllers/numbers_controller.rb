da62d93bde801edf6fcbed95a4df106a46390807
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-27 22:16:43 +0530

    Added

--- prime_factors_kata/app/controllers/numbers_controller.rb
+++ prime_factors_kata/app/controllers/numbers_controller.rb
@@ -85,14 +85,14 @@ class NumbersController < ApplicationController
     end
 
     #Fix Code
-    def generate(n)
+    def generate1(n)
     return [] if n == 1
     factor = (2..n).find {|x| n % x == 0} 
     [factor] + generate(n / factor) 
     end
 
     #Need fix here
-    def generate1(n)
+    def generate(n)
     return n
     end
 
